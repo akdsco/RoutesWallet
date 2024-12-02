3c22d5c0647692b571a033acb75c2059c5e3528c
akds <akds@pm.me>
2024-12-02 15:23:41 +0000

    feat: add sqlite to the project

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,8 @@ import { AppThemeProvider } from "@/providers/Theme";
 import * as SplashScreen from "expo-splash-screen";
 import { useLoading } from "@/hooks";
 import { StatusBar } from "expo-status-bar";
+import { SQLiteProvider } from "expo-sqlite";
+import { migrateDbIfNeeded } from "@/constants/dbInit";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync().then();
@@ -11,9 +13,11 @@ export default function RootLayout() {
   useLoading();
 
   return (
-    <AppThemeProvider>
-      <Slot />
-      <StatusBar style="auto" />
-    </AppThemeProvider>
+    <SQLiteProvider databaseName="local.db" onInit={migrateDbIfNeeded}>
+      <AppThemeProvider>
+        <Slot />
+        <StatusBar style="auto" />
+      </AppThemeProvider>
+    </SQLiteProvider>
   );
 }
---
+++ constants/dbInit.ts
@@ -0,0 +1,42 @@
+import { type SQLiteDatabase } from "expo-sqlite";
+
+export async function migrateDbIfNeeded(db: SQLiteDatabase) {
+  const DATABASE_VERSION = 1;
+
+  const userVersionObject = await db.getFirstAsync<{
+    user_version: number;
+  }>("PRAGMA user_version");
+
+  if (!userVersionObject) {
+    throw new Error("User database version not found");
+  }
+
+  let { user_version: currentDbVersion } = userVersionObject;
+
+  if (currentDbVersion >= DATABASE_VERSION) {
+    return;
+  }
+
+  if (currentDbVersion === 0) {
+    await db.execAsync(`
+      PRAGMA journal_mode = 'wal';
+      CREATE TABLE todos (id INTEGER PRIMARY KEY NOT NULL, value TEXT NOT NULL, intValue INTEGER);
+      `);
+    await db.runAsync(
+      "INSERT INTO todos (value, intValue) VALUES (?, ?)",
+      "hello",
+      1,
+    );
+    await db.runAsync(
+      "INSERT INTO todos (value, intValue) VALUES (?, ?)",
+      "world",
+      2,
+    );
+    currentDbVersion = 1;
+  }
+  // if (currentDbVersion === 1) {
+  //   Add more migrations
+  // }
+
+  await db.execAsync(`PRAGMA user_version = ${DATABASE_VERSION}`);
+}
