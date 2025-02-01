31cdc37434bd39feb26c8acb9c00e4ac43b94670
akds <akds@pm.me>
2025-02-01 11:26:22 +0000

    feat: move to useTagItem hook

---
+++ components/Tag/TagItem.hook.ts
@@ -0,0 +1,90 @@
+import { useState } from "react";
+import { log } from "@/library/logger";
+import { router } from "expo-router";
+import { Toast } from "@/library/Toast";
+import { TagItemProps } from "@/components/Tag/TagItem";
+
+const colors = [
+  "#FFB3B3", // pastel red
+  "red",
+  "#ADD8E6", // pastel blue
+  "blue",
+  "#B2F2B2", // pastel green
+  "green",
+  "#FFDAB3", // pastel orange
+  "orange",
+  "#DAB3FF", // pastel purple
+  "purple",
+  "#FFFFB3", // pastel yellow
+  "yellow",
+];
+
+export const useTagItem = (item: TagItemProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedName, setEditedName] = useState(item.name);
+  const [editedColor, setEditedColor] = useState(item.color);
+
+  const showTaggedRoutes = () => {
+    if (isEditing) {
+      // Currently editing, do not allow navigation (but we do allow navigation to other tags)
+      return;
+    }
+
+    log.debug("useTagItem", `Show \"${item.name}\" routes`);
+    router.push({
+      // @ts-ignore
+      pathname: `(tabs)/tags/${item.id}`,
+      params: { tagName: item.name },
+    });
+  };
+
+  const handleSelectedMenuOption = async (value: string) => {
+    log.debug(
+      "useTagItem",
+      `Menu selected option: ${value} tag \"${item.name}\", tagId: ${item.id}`,
+    );
+
+    switch (value) {
+      case "edit":
+        setIsEditing(true);
+        break;
+      case "remove":
+        await item.removeTag(item.id);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const saveChanges = async () => {
+    const name = editedName.trim();
+    if (name === "") {
+      Toast(
+        "error",
+        "Tag without a name",
+        "Try to make it slightly more descriptive",
+      );
+      return;
+    }
+
+    await item.updateTag(item.id, name, editedColor);
+    setIsEditing(false);
+  };
+
+  const onEditedTagPress = () => {
+    const nextColorIndex = (colors.indexOf(editedColor) + 1) % colors.length;
+    setEditedColor(colors[nextColorIndex]);
+  };
+
+  return {
+    isEditing,
+    showTaggedRoutes,
+    editedName,
+    setEditedName,
+    editedColor,
+    setEditedColor,
+    saveChanges,
+    handleSelectedMenuOption,
+    onEditedTagPress,
+  };
+};
--- components/Tag/TagItem.tsx
+++ components/Tag/TagItem.tsx
@@ -1,9 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import { StyleSheet, TextInput, TouchableOpacity, View } from "react-native";
 import { ThemedText } from "@/components/ThemedText";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
-import { useTheme } from "@/hooks";
-import { router } from "expo-router";
 import {
   Menu,
   MenuOption,
@@ -14,25 +12,10 @@ import {
   RenderItemParams,
   ScaleDecorator,
 } from "react-native-draggable-flatlist";
-import { log } from "@/library/logger";
-import { Toast } from "@/library/Toast";
 import { ThemeContextType } from "@/library/theme";
 import { RemoveTag, UpdateTag } from "@/library/types";
-
-const colors = [
-  "#FFB3B3", // pastel red
-  "red",
-  "#ADD8E6", // pastel blue
-  "blue",
-  "#B2F2B2", // pastel green
-  "green",
-  "#FFDAB3", // pastel orange
-  "orange",
-  "#DAB3FF", // pastel purple
-  "purple",
-  "#FFFFB3", // pastel yellow
-  "yellow",
-];
+import { useTagItem } from "@/components/Tag/TagItem.hook";
+import { useTheme } from "@/hooks";
 
 export type TagItemProps = {
   id: number;
@@ -47,60 +30,20 @@ export const TagItem = ({
   drag,
   isActive,
 }: RenderItemParams<TagItemProps>) => {
-  const themeContext = useTheme();
-  const [isEditing, setIsEditing] = useState(false);
-  const [editedName, setEditedName] = useState(item.name);
-  const [editedColor, setEditedColor] = useState(item.color);
+  const {
+    isEditing,
+    editedName,
+    setEditedName,
+    editedColor,
+    saveChanges,
+    showTaggedRoutes,
+    handleSelectedMenuOption,
+    onEditedTagPress,
+  } = useTagItem(item);
 
+  const themeContext = useTheme();
   const styles = makeStyles(themeContext);
   const { theme } = themeContext;
-
-  const showTaggedRoutes = () => {
-    if (isEditing) {
-      // Currently editing, do not allow navigation (but we do allow navigation to other tags)
-      return;
-    }
-
-    log.debug("useTagItem", `Show \"${item.name}\" routes`);
-    router.push({
-      // @ts-ignore
-      pathname: `(tabs)/tags/${item.id}`,
-      params: { tagName: item.name },
-    });
-  };
-
-  const handleSelectedMenuOption = async (value: string) => {
-    log.debug(
-      "useTagItem",
-      `Menu selected option: ${value} tag \"${item.name}\", tagId: ${item.id}`,
-    );
-
-    switch (value) {
-      case "edit":
-        setIsEditing(true);
-        break;
-      case "remove":
-        await item.removeTag(item.id);
-        break;
-      default:
-        break;
-    }
-  };
-
-  const saveChanges = async () => {
-    const name = editedName.trim();
-    if (name === "") {
-      Toast(
-        "error",
-        "Tag without a name",
-        "Try to make it slightly more descriptive",
-      );
-      return;
-    }
-
-    await item.updateTag(item.id, name, editedColor);
-    setIsEditing(false);
-  };
 
   return (
     <ScaleDecorator>
@@ -113,13 +56,7 @@ export const TagItem = ({
         <View style={styles.tagItemContainer}>
           <View style={styles.tagAndNameContainer}>
             {isEditing ? (
-              <TouchableOpacity
-                onPress={() => {
-                  const nextColorIndex =
-                    (colors.indexOf(editedColor) + 1) % colors.length;
-                  setEditedColor(colors[nextColorIndex]);
-                }}
-              >
+              <TouchableOpacity onPress={onEditedTagPress}>
                 <FontAwesome
                   size={32}
                   name="tag"
@@ -158,9 +95,7 @@ export const TagItem = ({
               <FontAwesome size={25} name="check" color="green" />
             </TouchableOpacity>
           ) : (
-            <Menu
-              onSelect={(value) => handleSelectedMenuOption(value as string)}
-            >
+            <Menu onSelect={(value: string) => handleSelectedMenuOption(value)}>
               <MenuTrigger style={styles.optionButtonContainer}>
                 <FontAwesome size={25} name="ellipsis-v" color={theme.text} />
               </MenuTrigger>
