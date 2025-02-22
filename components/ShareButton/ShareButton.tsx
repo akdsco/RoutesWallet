9be2ed67abb05fbe32abe56cfb1b03ae063e53ad
akds <akds@pm.me>
2025-02-22 09:47:12 +0000

    chore: use logger in ShareButton

--- components/ShareButton/ShareButton.tsx
+++ components/ShareButton/ShareButton.tsx
@@ -2,6 +2,7 @@ import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { useTheme } from "@/hooks";
 import { ParamListBase, RouteProp } from "@react-navigation/core";
 import { Share } from "react-native";
+import { log } from "@/library/logger";
 
 type ShareButtonProps = {
   route: RouteProp<ParamListBase, string>;
@@ -17,20 +18,22 @@ export const ShareButton = ({ route }: ShareButtonProps) => {
     const appMsg = `🚴‍♀️Strava route 🚴‍\nCheck out this route:\n`;
     const message = `https://www.strava.com/routes/${routeId}`;
 
+    const fnName = "ShareButton";
     try {
       const result = await Share.share({ message });
 
       if (result.action === Share.sharedAction) {
         if (result.activityType) {
-          console.log("Shared via", result.activityType);
+          log.info(fnName, `Shared route ${routeId}`, { result, route });
         } else {
-          console.log("Shared successfully");
+          log.info(fnName, "Shared successfully", { result, route });
         }
       } else if (result.action === Share.dismissedAction) {
-        console.log("Share dismissed");
+        log.info(fnName, "Share dismissed", { result, route });
       }
     } catch (error) {
-      console.error("Error sharing:", error);
+      log.error(fnName, "Error sharing", { error });
+      throw error;
     }
   };
 
