8f188d6d0e8158810692de33414345fe24cdf9f9
akds <akds@pm.me>
2025-02-23 23:17:30 +0000

    fix: use persistence "memory" to build with current buggy rn posthog

--- library/analytics/posthog.ts
+++ library/analytics/posthog.ts
@@ -3,5 +3,9 @@ import { AppConfig, isProduction } from "@/library/config";
 
 export const postHog = new PostHog(AppConfig.POSTHOG_API_KEY, {
   host: "https://eu.i.posthog.com",
+  // Why using "memory" persistence?
+  // https://github.com/PostHog/posthog-js-lite/issues/140
+  // https://chatgpt.com/share/67bbac5d-5370-800c-8e14-07bcf0e78716
+  persistence: "memory",
   disabled: !isProduction,
 });
