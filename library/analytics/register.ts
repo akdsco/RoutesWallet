1b38352369fd72844e369e462eec8aee26dcbd2c
akds <akds@pm.me>
2025-02-24 13:20:42 +0000

    feat: register user in Sentry

--- library/analytics/register.ts
+++ library/analytics/register.ts
@@ -2,6 +2,7 @@ import { PostHog } from "posthog-react-native";
 import { StravaAthleteBasic } from "@/integrations/strava";
 import { log } from "@/library/logger";
 import { isProduction } from "@/library/config";
+import * as Sentry from "@sentry/browser";
 
 export const registerUser =
   (postHog: PostHog) => async (athlete: StravaAthleteBasic) => {
@@ -9,15 +10,19 @@ export const registerUser =
       log.info("registerUser", "Registering user", { athlete });
     }
 
+    const fullName = `${athlete.firstname} ${athlete.lastname}`;
+
     postHog.identify(athlete.id.toString(), {
       $set: {
         user: athlete.username,
         firstName: athlete.firstname,
         lastName: athlete.lastname,
-        fullName: `${athlete.firstname} ${athlete.lastname}`,
+        fullName,
       },
       $set_once: {
         firstLogin: new Date(),
       },
     });
+
+    Sentry.setUser({ id: athlete.id, username: fullName });
   };
